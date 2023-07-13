399d135bcd9989a755f39ca825098f0faafa2a68
hivkey <1092176747@qq.com>
2023-07-13 10:55:54 +0800

    service_oss
    阿里云oss上传头像

---
+++ service/service_oss/src/main/java/com/atguigu/OssApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * @author WangTao
+ * @date 2023-07-12 21:08
+ * @description:
+ * @version:
+ */
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class OssApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OssApplication.class, args);
+    }
+}
